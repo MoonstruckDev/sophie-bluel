fc557714f89d20b8e0958a553e9fe260b7a84920
MoonstruckDev <dev@moonstruck.pro>
2024-02-01 14:22:35 +0100

    Added better index js

--- FrontEnd/scripts/index.js
+++ FrontEnd/scripts/index.js
@@ -1,28 +1,8 @@
-import { closeToast, closeToastDuration, createToast } from './toasts.js';
-import { getWorks, uploadPhoto, deleteWork } from './api.js';
-import { goHome, isLoggedIn, postLogin, showStatus } from './login-flow.js';
-import { displayWorks, updateGallery, resetWorks, gallery } from './works.js';
-import { isAdmin, getBearerToken, logout } from './admin.js';
-import { modalGallery } from './modal.js';
-
-
-export {
-    closeToast,
-    closeToastDuration,
-    createToast,
-    getWorks,
-    uploadPhoto,
-    deleteWork, 
-    goHome,
-    isLoggedIn, 
-    postLogin, 
-    showStatus,
-    displayWorks,
-    updateGallery,
-    resetWorks,
-    isAdmin,
-    getBearerToken,
-    logout,
-    gallery,
-    modalGallery
-  };
+  import "./homepage.js"
+  import "./api.js"
+  import "./works.js"
+  import "./works-filters.js"
+  import "./login-flow.js"
+  import "./admin.js"
+  import "./modal.js"
+  import "./toasts.js"
